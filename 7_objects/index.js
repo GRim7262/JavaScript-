bb40333d42cde72cdc4b5b68fdb0a45b6fba4cfc
GRim7262 <vinalpawar1@gmail.com>
2023-08-02 20:42:16 +0530

    Add existing project files to Git

--- 7_objects/index.js
+++ 7_objects/index.js
@@ -41,3 +41,69 @@
 // console.log(village);
 // console.log(aadharCardData.getFullName());
 // console.log(aadharCardData.getAadharNo());
+
+// Constructor Function
+// It start with capital letter
+
+// function Person(firstName, lastName, age) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.age = age;
+//   this.getFullName = function () {
+//     return this.firstName + "" + this.lastName + this.age;
+//   };
+// }
+
+// When we create objects using constructor function then we need to use new keyword before it
+// const person1 = new Person("Vinal", "Pawar", 23);
+// console.log(person1.getFullName());
+// console.log(student);
+
+// How to duplicate objects
+
+// const obj1 = { title: "obj1" };
+// const obj2 = obj1;
+
+// obj2.title = "obj2";
+
+// console.log("obj1", obj1);
+// console.log("obj2", obj2);
+
+// Spread Operator
+
+// const obj1 = { title: "obj1" };
+// const obj2 = { ...obj1 };
+
+// obj2.title = "obj2";
+
+// console.log("obj1", obj1);
+// console.log("obj2", obj2);
+
+const student = {
+  roll: 30,
+  firstName: "Vinal",
+  lastName: "Pawar",
+  class: 12,
+};
+
+console.log(student.hasOwnProperty("rboll"));
+
+// for in loop
+
+// for (let key in student) {
+//   console.log(key);
+// }
+
+// console.log(Object.entries(student));
+
+// Structure of map
+// [
+//   ["roll", 55]
+//   ["firstName", "Vinal"]
+//   ["lastName", "Pawar"]
+// ];
+
+// for (let [key, value] of Object.entries(student)) {
+//   console.log("key", key);
+//   console.log("value", value);
+// }
